75bea386a464d5eb756d3324ed2f81498b5182fa
Gareth Simpson <g@xurble.org>
2010-04-18 04:02:35 +0800

    Add weekly track chart to last.fm service

--- lib/Services/Lastfm.php
+++ lib/Services/Lastfm.php
@@ -72,6 +72,53 @@
 		}
 
 	}
+	
+	class LastFMWeeklyTracks extends LastFM {
+
+		public function __construct( $config ) {
+			parent::setVariables( $config );	
+			$this->classes = array( 'premier', 'deuxieme', 'troisieme', 'quatrieme' );
+
+			$this->setURL( sprintf( 'http://ws.audioscrobbler.com/2.0/?method=user.getweeklytrackchart&api_key=%s&user=%s', $this->key, $this->username ) );
+			$this->setItemTemplate('<li{%classe%}><a title="{%playcount%} plays" class="clearfix" href="{%link%}"><img src="{%image%}" width="{%size%}" height="{%size%}" alt="{%track%}"><strong><span>{%artist%}</span> {%track%}</strong></a></li>'."\n");
+
+			parent::__construct( $config );
+		}
+
+		/**
+		 * @return SimpleXMLElement
+		 */
+		public function getData() {
+			$data = parent::getData();
+			return $data->weeklytrackchart->track;
+		}
+
+		/**
+		 * @return array
+		 */
+		public function populateItemTemplate( &$item ) {
+			$album = $item->album;
+			$artist = $item->artist;
+			$title= $item->name;
+			$this->compteur++;
+			
+			$img = ($item->image[2] != '' ? $item->image[2] : Pubwich::getThemeUrl().'/img/cover.png');
+			
+			return array(
+						'link' => htmlspecialchars( $item->url ),
+						'artist' => $artist,
+						'album' => $album,
+						'track' => $title,
+						'date' => $item->date,
+						'image' => $img,
+						'size' => $this->size,
+						'playcount' => $item->playcount,
+						'classe' => isset($this->classes[$this->compteur-1]) ? ' class="'.$this->classes[$this->compteur-1].'"' : '',
+						);
+		}
+
+	}
+	
 
 	class LastFMWeeklyAlbums extends LastFM {
 
