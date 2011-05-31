23a36047d7ce743b5854cc5b473ae9589d6a8677
Michael Haschke <haschek@eye48.com>
2011-05-31 21:30:10 +0200

    [add] use meta class FeedMetaService for Atom and RSS, because of strong similarity [mod] RSS and Atom service class extends now FeedMetaService what extends Service

--- lib/Services/Atom.php
+++ lib/Services/Atom.php
@@ -1,25 +1,22 @@
 <?php
 	defined('PUBWICH') or die('No direct access allowed.');
+    require_once 'FeedMetaService.php';
 
 	/**
 	 * @classname Atom
 	 * @description Fetch Atom feeds
 	 * @version 1.1 (20090929)
 	 * @author Rémi Prévost (exomel.com)
+     * @author http://michael.haschke.biz/
 	 * @methods None
 	 */
 
-	class Atom extends Service {
+	class Atom extends FeedMetaService {
 
 		private $dateFormat;
 
 		public function __construct( $config ){
-			$this->total = $config['total'];
-			$this->dateFormat = $config['date_format'];
-			$this->setURL( $config['url'] );
 			$this->setHeaderLink( array( 'url' => $config['url'], 'type' => 'application/atom+xml' ) );
-			$this->setItemTemplate('<li><a href="{{link}}">{{{title}}}</a> {{{date}}}</li>'."\n");
-			$this->setURLTemplate( $config['link'] );
 			parent::__construct( $config );
 		}
 
@@ -33,55 +30,26 @@
 
 		/**
 		 * @return array
-		 */
-		public function populateItemTemplate( &$item ) {
-			return array(
-						'link' => $item['link'],
-						'title' => $item['title'],
-						'date' => $item['date'],
-						'absolute_date' => $item['absolute_date'],
-						'content' => $item['content'],
-			);
-		}
-
-		/**
-		 * @return array
-         * @since 20110531
-		 */
-        public function processDataStream() {
-            $data_source = $this->getData();
-            $data_processed = array();
-
-            if (!$data_source) return false;
-            if (!method_exists($this, 'processDataItem')) return false;
-
-            foreach ($data_source as $data_item) {
-                $data_processed[] = $this->processDataItem($data_item);
-            }
-
-            return $data_processed;
-
-        }
-
-		/**
-		 * @return array
          * @since 20110531
 		 */
         public function processDataItem( &$item ) {
 			$link = htmlspecialchars($item->link->attributes()->href);
-			$date = $item->published ? $item->published : $item->updated;
             $title = strip_tags(trim( $item->title ));
+            $author = trim($item->author->name);
+			$date = $item->published ? $item->published : $item->updated;
             $summary = strip_tags(trim( str_replace(array('<br>','<br/>'), ' ', $item->summary) ));
             $content = trim($item->content);
-            $author = trim($item->author->name);
+
             if (!$title) $title = $summary ? $summary : strip_tags(str_replace(array('<br>','<br/>'), ' ', $content));
             if (strlen($title) > 140) $title = substr($title, 0, 140).'...';
-			return array(
+
+            return array(
 						'link' => $link,
 						'title' => $title,
                         'author' => $author,
 						'date' => Pubwich::time_since( $date ),
 						'absolute_date' => date($this->dateFormat, strtotime($date)),
+                        'summary' => $summary,
 						'content' => $content,
 			);
             
--- lib/Services/RSS.php
+++ lib/Services/RSS.php
@@ -1,24 +1,21 @@
 <?php
 	defined('PUBWICH') or die('No direct access allowed.');
+    require_once 'FeedMetaService.php';
 
 	/**
 	 * @classname RSS
 	 * @description Fetch RSS feeds
 	 * @version 1.1 (20090929)
 	 * @author Rémi Prévost (exomel.com)
+     * @author http://michael.haschke.biz/
 	 * @methods None
 	 */
 
-	class RSS extends Service {
+	class RSS extends FeedMetaService {
 
 		private $dateFormat;
 
 		public function __construct( $config ){
-			$this->setURL( $config['url'] );
-			$this->total = $config['total'];
-			$this->dateFormat = $config['date_format'];
-			$this->setItemTemplate('<li><a href="{{{link}}}">{{{title}}}</a> {{{date}}}</li>'."\n");
-			$this->setURLTemplate( $config['link'] );
 			$this->setHeaderLink( array( 'url' => $config['url'], 'type' => 'application/rss+xml' ) );
 			parent::__construct( $config );
 		}
@@ -40,21 +37,32 @@
 
 		/**
 		 * @return array
+         * @since 20110531
 		 */
-		public function populateItemTemplate( &$item ) {
+        public function processDataItem( &$item ) {
+			$link = htmlspecialchars($item->link);
+            $title = strip_tags(trim( $item->title ));
+            $author = trim($item->author);
+			$date = $item->pubDate;
+            $summary = strip_tags(trim( str_replace(array('<br>','<br/>'), ' ', $item->description) ));
+            $content = trim($item->children('http://purl.org/rss/1.0/modules/content/')->encoded);
+            $comments_link = $item->comments;
 			$comments_count = $item->children('http://purl.org/rss/1.0/modules/slash/')->comments;
-			$content = $item->children('http://purl.org/rss/1.0/modules/content/')->encoded;
-			return array(
-						'link' => htmlspecialchars( $item->link ),
-						'title' => trim( $item->title ),
-						'date' => Pubwich::time_since( $item->pubDate ),
-						'absolute_date' => date($this->dateFormat, strtotime($item->pubDate)),
-						'comments_link' => $item->comments,
-						'comments_count' => $comments_count,
-						'description' => $item->description,
-						'content' => $content,
-						'author' => $item->author,
-			);
-		}
 
+            if (!$title) $title = $summary ? $summary : strip_tags(str_replace(array('<br>','<br/>'), ' ', $content));
+            if (strlen($title) > 140) $title = substr($title, 0, 140).'...';
+
+            return array(
+						'link' => $link,
+						'title' => $title,
+                        'author' => $author,
+						'date' => Pubwich::time_since( $date ),
+						'absolute_date' => date($this->dateFormat, strtotime($date)),
+                        'summary' => $summary,
+						'content' => $content,
+						'comments_link' => $comments_link,
+						'comments_count' => $comments_count,
+			);
+
+        }
 	}
