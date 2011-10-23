da2b55f38bbc559efc46a2c6877d900687883150
Michael Haschke <haschek@eye48.com>
2011-10-23 14:58:52 +0200

    [mod] some light weight changes to the default template

--- themes/default/index.tpl.php
+++ themes/default/index.tpl.php
@@ -1,8 +1,9 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title><?php echo PUBWICH_TITLE?></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="generator" content="<?php echo PUBWICH_NAME;?> <?php echo PUBWICH_VERSION;?>" />
 		<link rel="stylesheet" media="screen" href="<?php echo Pubwich::getThemeUrl()?>/style.css" type="text/css">
 		<link type="text/plain" rel="author" href="humans.txt" />
 <?php echo Pubwich::getHeader() ?>
@@ -19,7 +20,7 @@
 			<div id="footer">
 				<div class="footer-inner">
 					<hr>
-					<?php echo sprintf( Pubwich::_('All data is &copy; copyright %s. Proudly powered by %s.'), date('Y'), '<a class="pubwich" href="http://pubwich.org/">Pubwich</a>'  )?>
+					<?php echo sprintf( Pubwich::_('All data is &copy; copyright %s. Proudly powered by %s.'), date('Y'), '<a class="pubwich" href="'.PUBWICH_WEB.'">'.PUBWICH_NAME.'</a>'  )?>
 				</div>
 			</div>
 		</div>
