7e29b74e1f5c44f8dab92574a5100632d9f4048e
Michael Haschke <haschek@eye48.com>
2011-11-13 20:42:26 +0100

    [add] add example filter to shorten title and summary texts from Feeds

--- themes/default/filters.sample.php
+++ themes/default/filters.sample.php
@@ -4,7 +4,7 @@
  * - rename it to filters.php
  * - add filters
  *
- * Filters only work if the Service class has processDataStream() implemented!
+ * Filters only work if the Service class has processDataStream() and processDataItem() implemented!
  *
  * Name convention for functions:
  *
@@ -15,9 +15,9 @@
  * ServiceParentClass_ServiceClass_ServiceConfigID_filterStream(ServiceParentClass object)
  * ServiceParentClass_ServiceClass_ServiceConfigID_filterItem(item of ProcessedDataArray)
  *
- * e.g.: Service_filterStream($service)
- *       Service_RSS_filterItem($item)
- *       Service_Atom_weblog_filterItem($item)
+ * e.g.: Feed_filterStream($service)
+ *       Feed_RSS_filterItem($item)
+ *       Feed_Atom_weblog_filterItem($item)
  *
  */
 
@@ -34,3 +34,61 @@ function Service_filterItem($item)
     // this $item is a item of the already processed data
 }
 */
+
+function Feed_filterItem($item)
+{
+    $item['title'] = shortenString(
+        $item['title'],
+        175
+    );
+    $item['summary'] = shortenString(
+        strip_tags(str_replace(array('<br>','<br/>'), ' ', $item['summary'])),
+        250
+    );
+
+    return;
+}
+
+function shortenString($text, $maxlength, $endchars = array('.','!','?'), $fallbackendchars = array(',',' '))
+{
+    if (strlen($text) <= $maxlength) return $text;
+
+    $text = substr($text, 0, $maxlength);
+
+    // use configured ending as finishing character (if available)
+    $lastpos = array();
+    foreach($endchars as $endchar)
+    {
+        $lastpos[$endchar] = strrpos($text, $endchar)!==false ? strrpos($text, $endchar) : 0;
+    }
+
+    $cuttextatpos = max($lastpos);
+
+    // use fallback char if no finishing char is found
+    if ($cuttextatpos == 0)
+    {
+        foreach($fallbackendchars as $fallbackendchar)
+        {
+            if (strrpos($text, $fallbackendchar)!==false)
+            {
+                $cuttextatpos = strrpos($text, $fallbackendchar);
+                break;
+            }
+
+        }
+    }
+
+    if ($cuttextatpos == 0) // still, even after checking fallbacks
+    {
+        $cutbodyatpos = $maxlength;
+    }
+    else
+    {
+        $cuttextatpos++;
+    }
+
+
+    return substr($text, 0, $cuttextatpos);
+
+}
+
