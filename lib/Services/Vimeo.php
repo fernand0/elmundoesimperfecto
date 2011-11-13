178d57b1c31a65544420d0bcecbcf3bde09b0ed9
Michael Haschke <haschek@eye48.com>
2011-11-13 22:57:27 +0100

    [mod] adjust vimeo service class (using Feed now) and default styles for vimeo

--- lib/Services/Vimeo.php
+++ lib/Services/Vimeo.php
@@ -9,43 +9,23 @@
 	 * @methods None
 	 */
 
-	class Vimeo extends Service {
+    Pubwich::requireServiceFile( 'Feed' );
+	class Vimeo extends Feed {
 
 		public function __construct( $config ){
-			$this->setURL( sprintf( 'http://vimeo.com/api/%s/clips.xml', $config['username'] ) );
-			$this->total = $config['total'];
-			$this->username = $config['username'];
-
-			$this->setItemTemplate('<li><a class="clearfix" href="{{{link}}}"><img src="{{{image_small}}}" alt="{{{title}}}" /><span>{{{title}}}</span></a></li>'."\n");
-			$this->setURLTemplate('http://www.vimeo.com/'.$config['username'].'/');
+			$config['list'] = isset($config['list']) ? $config['list']:'videos';
+            $config['url'] = sprintf( 'http://vimeo.com/%s/%s/rss', $config['username'], $config['list'] );
+			$config['link'] = 'http://www.vimeo.com/'.$config['username'].'/';
+            $config['contenttype'] = 'application/rss+xml';
 
 			parent::__construct( $config );
-		}
 
-		/**
-		 * @return SimpleXMLElement
-		 */
-		public function getData() {
-			$data = parent::getData();
-			return $data->clip;
-		}
-
-		/**
-		 * @return array
-		 */
-		public function populateItemTemplate( &$item ) {
-			return array(
-						'link' => htmlspecialchars( $item->url ),
-						'title' => $item->title,
-						'date' => Pubwich::time_since( $item->uploaded_date ),
-						'caption' => $item->caption,
-						'duration' => $item->duration,
-						'width' => $item->width,
-						'height' => $item->height,
-						'image_small' => $item->thumbnail_small,
-						'image_medium' => $item->thumbnail_medium,
-						'image_large' => $item->thumbnail_large,
-			);
+			$this->setItemTemplate(
+                '<li>
+                    <a href="{{{link}}}"><img src="{{{media_thumbnail_url}}}" alt="" class="item-media-thumbnail" height="75"/>
+                    {{{title}}}</a> ({{{date}}})
+                 </li>'."\n"
+            );
 		}
 
 	}
