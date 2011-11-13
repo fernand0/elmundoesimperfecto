07a7364467a51770651b47c3dd10a3eb5844f5ab
Michael Haschke <haschek@eye48.com>
2011-11-14 00:40:09 +0100

    [mod] adjust Youtube service class + styles
    - remove youtube classes for method parameter
    - add new list parameter (uploads, favorites)
    - using Feed class

--- lib/Services/Youtube.php
+++ lib/Services/Youtube.php
@@ -2,62 +2,35 @@
 	defined('PUBWICH') or die('No direct access allowed.');
 
 	/**
-	 * @classname Youtube
-	 * @description Fetch Youtube videos
+	 * Youtube Service
+     *
+	 * Fetch Youtube videos.
+     *
 	 * @version 1.2 (20100530)
 	 * @author Rémi Prévost (exomel.com)
-	 * @methods YoutubeVideos YoutubeVideos
+     * @author Michael Haschke, http://michael.haschke.biz/
 	 */
 
-	Pubwich::requireServiceFile( 'Atom' );
-	class Youtube extends Atom {
-
-		private $size;
+	Pubwich::requireServiceFile( 'Feed' );
+	class Youtube extends Feed {
 
 		public function __construct( $config ){
-			$this->size = $config['size'];
+			$config['list'] = isset($config['list']) ? $config['list']:'uploads';
+            $config['url'] = 'http://gdata.youtube.com/feeds/api/users/'.
+                             $config['username'].'/'.$config['list'].
+                             '?v=2&orderby=published&max-results='.$config['total'];
+			$config['link'] = 'http://youtube.com/user/'.$config['username'];
+            $config['contenttype'] = 'application/atom+xml';
+
 			parent::__construct( $config );
-			$this->setItemTemplate('<li class="clearfix"><a href="{{{link}}}"><img src="{{{image}}}" alt="{{{title}}}" /><strong>{{{title}}}</strong></a></li>'."\n");
-		}
 
-		/**
-		 * @return array
-		 */
-		public function populateItemTemplate( &$item ) {
-			$media = $item->children('http://search.yahoo.com/mrss/');
-
-			if ( $media->group->thumbnail ) {
-				$attrs = $media->group->thumbnail[0]->attributes();
-				$title = (string) $media->group->title;
-				$description = (string) $media->group->description;
-				$url_attrs = $media->group->player->attributes();
-				$url = $url_attrs['url'];
-			}
-
-			return parent::populateItemTemplate( $item ) + array(
-				'image' => $attrs ? $attrs['url'] : '',
-				'size' => $item->size,
-			);
+			$this->setItemTemplate(
+                '<li>
+                    <a href="{{{link}}}"><img src="{{{media_thumbnail_url}}}" alt="" class="item-media-thumbnail" height="75"/>
+                    {{{title}}}</a> ({{{date}}})
+                 </li>'."\n"
+            );
 		}
 
 	}
 
-	class YoutubeVideos extends Youtube {
-
-		public function __construct( $config ){
-			$config['url'] = sprintf( 'http://gdata.youtube.com/feeds/api/users/%s/uploads?v=2', $config['username'] );
-			$config['link'] = 'http://www.youtube.com/user/'.$config['username'].'/';
-			parent::__construct( $config );
-		}
-
-	}
-
-	class YoutubeFavorites extends Youtube {
-
-		public function __construct( $config ){
-			$config['url'] = sprintf( 'http://gdata.youtube.com/feeds/api/users/%s/favorites?v=2', $config['username'] );
-			$config['link'] = 'http://www.youtube.com/user/'.$config['username'].'#p/f';
-			parent::__construct( $config );
-		}
-
-	}
