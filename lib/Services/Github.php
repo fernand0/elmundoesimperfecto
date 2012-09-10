c164a3a53643c14ab8d1f49fcd6da975f14fcba4
Michael Haschke <haschek@eye48.com>
2012-09-10 02:01:47 +0200

    [fix] test and correct Github service, close #15

--- lib/Services/Github.php
+++ lib/Services/Github.php
@@ -4,88 +4,72 @@
 	/**
 	 * @classname GitHub
 	 * @description Fetch GitHub user public activity feed
-	 * @version 1.2 (20100530)
 	 * @author Rémi Prévost (exomel.com)
+	 * @author http://michael.haschke.biz/
 	 * @methods GithubRecentActivity GithubRepositories
 	 */
 
-	Pubwich::requireServiceFile( 'Atom' );
-	class GithubRecentActivity extends Atom {
+	Pubwich::requireServiceFile( 'Feed' );
+	class GithubRecentActivity extends Feed {
 
 		/**
 		 * @constructor
 		 */
 		public function __construct( $config ){
-			$config['url'] = sprintf( 'http://github.com/%s.atom', $config['username'] );
-			$config['link'] = 'http://github.com/'.$config['username'].'/';
+			$config['url'] = sprintf( 'https://github.com/%s.atom', $config['username'] );
+			$config['link'] = 'https://github.com/'.$config['username'];
+			$config['contenttype'] = 'application/atom+xml';
 			parent::__construct( $config );
-			$this->setItemTemplate('<li class="clearfix"><a href="{{{link}}}">{{{title}}}</a></li>'."\n");
-		}
-
-		/**
-		 * @return array
-		 */
-		public function populateItemTemplate( &$item ) {
-			return parent::populateItemTemplate( $item ) + array(
-			);
-		}
-
-		/**
-		 * Adds "github" to the box HTML class attribute
-		 * @return array
-		 */
-		public function populateBoxTemplate( $data ) {
-			return parent::populateBoxTemplate( $data ) + array( 'class' => $data['class'].' github');
+			$this->setItemTemplate('<li><a href="{{{link}}}">{{{title}}}</a> ({{{date}}})</li>'.PHP_EOL);
 		}
 
 	}
-
+    
 	class GithubRepositories extends Service {
 
 		/**
 		 * @constructor
 		 */
 		public function __construct( $config ){
-			$this->setURL( sprintf( 'http://github.com/api/v2/xml/repos/show/%s', $config['username'] ) );
-			$this->setURLTemplate( 'http://github.com/'.$config['username'].'/' );
-			$this->setItemTemplate('<li class="clearfix"><div class="metadata"><a href="{{{url}}}"><strong>{{{name}}}</strong></a>{{#fork}}<span class="fork">(fork)</span>{{/fork}}</div><div class="infos">{{{description}}}</div></li>'."\n");
+		    if (!isset($config['sort'])) $config['sort'] = 'updated';
+		    $this->sorted_by = $config['sort'];
+		    if (!isset($config['ownertype'])) $config['ownertype'] = 'users';
+            $this->total = $config['total'];
+			$this->setURL( sprintf( 'https://api.github.com/%s/%s/repos?type=owner&sort=%s', $config['ownertype'], $config['owner'], $config['sort'] ) );
+			$this->setURLTemplate( 'https://github.com/'.$config['owner'] );
+			$this->setItemTemplate('<li><a href="{{{link}}}">{{{name}}}</a> {{#fork}}(forked){{/fork}}<br/>{{{description}}} ({{{date}}})</li>'.PHP_EOL);
 			parent::__construct( $config );
+			$this->callback_function = array('Pubwich', 'json_decode');
 		}
 
-		/**
-		 * @return array
-		 */
 		public function populateItemTemplate( &$item ) {
-			$fork = $item->fork == 'true';
-			$private = $item->private == 'true';
-			return array(
-				'description' => $item->description,
-				'forks' => $item->forks,
-				'name' => $item->name,
-				'watchers' => $item->watchers,
-				'private' => $private,
-				'url' => $item->url,
-				'fork' => $fork,
-				'owner' => $item->owner,
-				'homepage' => $item->homepage,
-			);
+			return $item;
 		}
 
-		/**
-		 * @return array
-		 */
-		public function getData() {
-			return parent::getData()->repository;
-		}
+        /**
+         * @return array
+         * @since 20120909
+         */
+        public function processDataItem($item) {
 
-		/**
-		 * Adds "github" to the box HTML class attribute
-		 * @return array
-		 */
-		public function populateBoxTemplate( $data ) {
-			return parent::populateBoxTemplate( $data ) + array( 'class' => $data['class'].' github');
-		}
-
+            $date_used = ($this->sorted_by !== 'full_name') ? $this->sorted_by . '_at' : 'created_at';
+            $date = $item->$date_used;
+            
+            return array(
+                'name' => $item->name,
+                'description' => $item->description,
+                'owner' => $item->owner->login,
+                'link' => $item->html_url,
+                'language' => $item->language,
+                'homepage' => $item->homepage,
+                'fork' => $item->fork,
+                'stars' => $item->watchers,
+                'forks' => $item->forks,
+                'issues' => $item->open_issues,
+                'date' => Pubwich::time_since($date),
+                'timestamp' => strtotime($date),
+            );
+        }
 	}
 
 	class GithubGists extends Service {
@@ -94,41 +78,32 @@
 		 * @constructor
 		 */
 		public function __construct( $config ){
-			$this->setURL( sprintf( 'http://gist.github.com/api/v1/xml/gists/%s', $config['username'] ) );
-			$this->setURLTemplate( 'http://gist.github.com/'.$config['username'].'/' );
-			$this->setItemTemplate('<li class="clearfix"><a href="{{permalink}}">{{#files}}{{{file}}}{{/files}}</a> {{{description}}} {{{date}}}</li>'."\n");
+            $this->total = $config['total'];
+			$this->setURL( sprintf( 'https://api.github.com/users/%s/gists', $config['owner'] ) );
+			$this->setURLTemplate( 'https://gist.github.com/'.$config['owner'] );
+			$this->setItemTemplate('<li><a href="{{link}}">{{{description}}}</a> ({{{date}}})</li>'.PHP_EOL);
 			parent::__construct( $config );
+			$this->callback_function = array('Pubwich', 'json_decode');
 		}
 
-		/**
-		 * @return array
-		 */
 		public function populateItemTemplate( &$item ) {
-			$public = $item->fork == 'true';
-			return array(
-				'description' => $item->description,
-				'repo' => $item->repo,
-				'permalink' => sprintf( 'http://gist.github.com/%s', $item->repo ),
-				'date' => Pubwich::time_since( $item->{'created-at'} ),
-				'owner' => $item->owner,
-				'files' => (array) $item->files->file,
-			);
+            return $item;
 		}
+		
+        /**
+         * @return array
+         * @since 20120909
+         */
+        public function processDataItem($item) {
 
-		/**
-		 * @return array
-		 */
-		public function getData() {
-			return parent::getData()->gist;
-		}
-
-		/**
-		 * Adds "github" to the box HTML class attribute
-		 * @return array
-		 */
-		public function populateBoxTemplate( $data ) {
-			return parent::populateBoxTemplate( $data ) + array( 'class' => $data['class'].' github');
-		}
-
+            return array(
+                'description' => $item->description,
+                'owner' => $item->user->login,
+                'link' => $item->html_url,
+                'public' => $item->public,
+                'date' => Pubwich::time_since($item->updated_at),
+                'timestamp' => strtotime($item->updated_at),
+            );
+        }
 	}
 
